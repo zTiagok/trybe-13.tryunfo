55523064742ba6b2b072736f0a2418366b62e258
Tiago <ztiagok@gmail.com>
2022-06-24 16:45:07 -0500

    c4 - 4º Requisito Iniciado

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+    };
+  }
+
+  changeState = () => {
+
+  }
+
   render() {
     return (
       <div>
         <h1>Valorant Trunfo</h1>
-        <Form />
+        <Form onInputChange={ this.changeState } />
         <Card />
       </div>
     );
